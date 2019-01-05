e35b047fbe60fb95fee9a4331e84f394f3ac460b
bhanuraghav <bhanuraghav1999@gmail.com>
2019-01-05 20:19:20 +0530

    Add form for messages and show  incoming messages in browser

--- public/js/index.js
+++ public/js/index.js
@@ -8,10 +8,24 @@ socket.on('disconnect',function(){
 
 socket.on('newMessage', function(message){
     console.log("New Message",message);
+    var li = jQuery('<li></li>');
+    li.text(`${message.from}: ${message.text}`);
+    jQuery('#messages').append(li);
 })
+ 
+// socket.emit('createMessage',{
+//     from : 'Bhanu',
+//     text : 'hello bhanu'
+// },function(data){
+//     console.log('Got it :', data);
+// }) 
 
-  // socket.emit('createMessage',{
-    //     // to:'abc@gmail.com',
-    //     from : 'Bhanu',
-    //     text : 'hello bhanu'
-    // })
+jQuery('#message-form').on('submit',function(e){
+    e.preventDefault();
+    socket.emit('createMessage',{
+        from : 'User',
+        text: jQuery('[name=message]').val()
+    },function(){
+
+    })
+})
