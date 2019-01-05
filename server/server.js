a5a5c2d97e7eb9c9307f84a1c7598cfe531a5d51
bhanuraghav <bhanuraghav1999@gmail.com>
2019-01-05 14:00:44 +0530

    Broadcasting events

--- server/server.js
+++ server/server.js
@@ -14,19 +14,31 @@ app.use(express.static(publicPath));
 io.on('connection',(socket)=>{
     console.log('New User connected');
    
-    // socket.emit('newMessage', {
-    //     from: "Raghav",
-    //     text : "Hello Bhanu",
-    //     createdAt : 1234
-    // })
+    //socket.emit from Admin text welcome to chat room
+    socket.emit('newMessage', {
+        from: "Admin",
+        text : "welcome to chat room",
+        createdAt : new Date().getTime()
+    })
+
+    socket.broadcast.emit('newMessage',{
+        from: "Admin",
+        text : "New User Joined",
+        createdAt : new Date().getTime()
+    })
 
     socket.on('createMessage',(message)=>{
         console.log('createMessage',message);
-        io.emit('newMessage',{
-            from : message.from,
-            text : message.text,
-            createdAt : new Date().getTime()
-        })
+        // io.emit('newMessage',{
+        //     from : message.from,
+        //     text : message.text,
+        //     createdAt : new Date().getTime()
+        // })
+        // socket.broadcast.emit('newMessage',{
+        //     from : message.from,
+        //     text : message.text,
+        //     createdAt : new Date().getTime()
+        // })
     })
 
     socket.on('disconnect',()=>{
