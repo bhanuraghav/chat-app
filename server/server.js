8e723132c320dfd99df56e63ca13830256ccfccc
bhanuraghav <bhanuraghav1999@gmail.com>
2019-01-06 01:26:07 +0530

    Geolocation utils added

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const express =require('express');
 const socketIO =require('socket.io');
 const http = require('http');
 const app =express();
-const {generateMessage} = require('./utils/message');
+const {generateMessage,generateLocationMessage} = require('./utils/message');
 
 
 const port =process.env.PORT || 3000;
@@ -32,6 +32,9 @@ io.on('connection',(socket)=>{
         callback('This if from server');
     })
  
+    socket.on('createLocationMessage',(coords)=>{
+        io.emit('newLocationMessage',generateLocationMessage('Admin',coords.latitude,coords.longitude))
+    })
     socket.on('disconnect',()=>{
         console.log('User was disconnected');
     })
